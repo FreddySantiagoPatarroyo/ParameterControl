40dfc145e463fa4d73ffd26cafc7bba9d0907368
FreddyPatarroyo.Col <FreddyPatarroyo.Col@sqdm.com>
2023-11-30 11:54:07 -0500

    incio de proyecto

---
+++ ParameterControl/Controllers/Login/LoginController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ParameterControl.Models.Login;
+using System.Xml.Serialization;
+
+namespace ParameterControl.Controllers.Login
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel request)
+        {
+            var user = request;
+            return View();
+        }
+    }
+}
---
+++ ParameterControl/Models/Login/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ParameterControl.Models.Login
+{
+    public class LoginViewModel
+    {
+        public string user { get; set; } = string.Empty;
+        public string password {  get; set; } = string.Empty;
+    }
+}
