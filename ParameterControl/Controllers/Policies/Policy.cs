fcb74928c6b3d420f909760229431561321aac62
FreddyPatarroyo.Col <FreddyPatarroyo.Col@sqdm.com>
2023-12-04 11:53:24 -0500

    estrucutra para las tablas

---
+++ ParameterControl/Controllers/Policies/Policy.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ParameterControl.Controllers.Policies
+{
+    public class Policy : Controller
+    {
+        public ActionResult Policies()
+        {
+            return View();
+        }
+    }
+}
---
+++ ParameterControl/Models/Policies/Policy.cs
@@ -0,0 +1,12 @@
+﻿namespace ParameterControl.Models.Policies
+{
+    public class Policy
+    {
+        public string Code { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int Conciliation { get; set; }
+        public string ControlType { get; set; } = string.Empty;
+        public string OperationType { get; set; } = string.Empty;
+    }
+}
