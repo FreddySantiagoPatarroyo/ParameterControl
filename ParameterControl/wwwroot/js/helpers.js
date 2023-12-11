0831e46e1a3bf547ee338751c1c6a527068ccca1
FreddyPatarroyo.Col <FreddyPatarroyo.Col@sqdm.com>
2023-12-11 14:55:01 -0500

    ajustes para filtros en la tabla de politicas

---
+++ ParameterControl/wwwroot/js/helpers.js
@@ -0,0 +1,14 @@
+﻿let viewOptionsFilter = false;
+
+const showOptions = () => {
+
+    viewOptionsFilter = !viewOptionsFilter;
+
+    if (viewOptionsFilter) {
+        document.getElementById('options').style.display = 'block';
+        document.getElementById('select_helper').style.display = 'block';
+    } else {
+        document.getElementById('options').style.display = 'none';
+        document.getElementById('select_helper').style.display = 'none';
+    }
+}
