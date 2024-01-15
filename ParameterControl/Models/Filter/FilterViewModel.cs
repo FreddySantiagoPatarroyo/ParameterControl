399d1c05cc15d575c953e1bbd755e150f3ae2a78
FreddyPatarroyo.Col <FreddyPatarroyo.Col@sqdm.com>
2024-01-15 15:44:03 -0500

    ajustes en el filtro

--- ParameterControl/Models/Filter/FilterViewModel.cs
+++ ParameterControl/Models/Filter/FilterViewModel.cs
@@ -1,10 +1,13 @@
 ﻿ using ParameterControl.Models.Rows;
+using System.ComponentModel.DataAnnotations;
 
 namespace ParameterControl.Models.Filter
 {
     public class FilterViewModel
     {
+        [Required(ErrorMessage = "Este campo es requerido")]
         public string ColumValue { get; set; } =  string.Empty;
+        [Required(ErrorMessage = "Este campo es requerido")]
         public string ValueFilter { get; set; } = string.Empty;
         public List<Row> Rows { get; set; } = new List<Row>();
     }
