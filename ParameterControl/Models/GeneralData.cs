dac51a902fbdf142b66695ea12d483470f2e7ba0
FreddyPatarroyo.Col <FreddyPatarroyo.Col@sqdm.com>
2024-01-25 17:06:11 -0500

    ajustes en la fecha

--- ParameterControl/Models/GeneralData.cs
+++ ParameterControl/Models/GeneralData.cs
@@ -4,9 +4,9 @@ namespace ParameterControl.Models
 {
     public class GeneralData
     {
-        [DataType(DataType.Date)]
+        [DataType(DataType.DateTime)]
         public DateTime UpdateDate { get; set; } = DateTime.MinValue;
-        [DataType(DataType.Date)]
+        [DataType(DataType.DateTime)]
         public DateTime CreationDate { get; set; } = DateTime.MinValue;
         public int UserOwner { get; set; }
         public bool State { get; set; } = false;
